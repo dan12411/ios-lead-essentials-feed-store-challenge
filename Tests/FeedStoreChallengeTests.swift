3ae047f6f60cec4e06479e08d0fdfebd13a34ff4
Dan Hung <dan12411@gmail.com>
2020-10-29 22:14:33 +0800

    Delete command delivers no error on empty cache

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -26,7 +26,7 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
         private var cache: Cache?
 
         func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-
+            completion(nil)
         }
 
         func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
@@ -86,9 +86,9 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 	}
 
 	func test_delete_deliversNoErrorOnEmptyCache() {
-//		let sut = makeSUT()
-//
-//		assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
+		let sut = makeSUT()
+
+		assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
 	}
 
 	func test_delete_hasNoSideEffectsOnEmptyCache() {
