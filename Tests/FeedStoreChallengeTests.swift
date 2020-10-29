04813f99ff69fb2eeb173d7ba70e4e656872f314
Dan Hung <dan12411@gmail.com>
2020-10-29 22:19:08 +0800

    Delete command empties previously inserted cache

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -26,6 +26,7 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
         private var cache: Cache?
 
         func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+            cache = nil
             completion(nil)
         }
 
@@ -104,9 +105,9 @@ class FeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
 	}
 
 	func test_delete_emptiesPreviouslyInsertedCache() {
-//		let sut = makeSUT()
-//
-//		assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
+		let sut = makeSUT()
+
+		assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
 	}
 
 	func test_storeSideEffects_runSerially() {
