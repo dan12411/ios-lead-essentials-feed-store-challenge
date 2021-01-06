df7b24f00a4bb7e94ba1d1b413bd44ec8a50685a
Dan Hung <dan12411@gmail.com>
2021-01-06 23:36:20 +0800

    fix: Delete `DispatchQueue` for test failure issue

--- FeedStoreChallenge/RealmFeedStore.swift
+++ FeedStoreChallenge/RealmFeedStore.swift
@@ -4,24 +4,9 @@ import RealmSwift
 public final class RealmFeedStore {
 
 	private let configuration: Realm.Configuration
-	private let dispatchQueue = DispatchQueue(label: "\(RealmFeedStore.self)Queue", attributes: .concurrent)
 
 	public init(configuration: Realm.Configuration) {
 		self.configuration = configuration
-	}
-
-	private func performWithBarrier(_ operation: @escaping (Realm.Configuration) -> Void) {
-		dispatchQueue.async(flags: .barrier) { [weak self] in
-			guard let self = self else { return }
-			operation(self.configuration)
-		}
-	}
-
-	private func perform(_ operation: @escaping (Realm.Configuration) -> Void) {
-		dispatchQueue.async { [weak self] in
-			guard let self = self else { return }
-			operation(self.configuration)
-		}
 	}
 }
 
@@ -34,58 +19,52 @@ extension RealmFeedStore: FeedStore {
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
 		if configuration.readOnly { return completion(StoreError.readOnly) }
 
-		performWithBarrier { (configuration) in
-			do {
-				let realm = try Realm(configuration: configuration)
-				try realm.write {
-					let cachedFeed = realm.objects(Cache.self)
-					realm.delete(cachedFeed)
-				}
-				completion(nil)
-
-			} catch {
-				completion(error)
+		do {
+			let realm = try Realm(configuration: configuration)
+			try realm.write {
+				let cachedFeed = realm.objects(Cache.self)
+				realm.delete(cachedFeed)
 			}
+			completion(nil)
+
+		} catch {
+			completion(error)
 		}
 	}
 
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
 		if configuration.readOnly { return completion(StoreError.readOnly) }
 
-		performWithBarrier { (configuration) in
-			do {
-				let realm = try Realm(configuration: configuration)
+		do {
+			let realm = try Realm(configuration: configuration)
 
-				try realm.write {
-					let cachedFeed = realm.objects(Cache.self)
-					realm.delete(cachedFeed)
+			try realm.write {
+				let cachedFeed = realm.objects(Cache.self)
+				realm.delete(cachedFeed)
 
-					let items = feed.map { ImageItem($0) }
-					let cache = Cache()
+				let items = feed.map { ImageItem($0) }
+				let cache = Cache()
 
-					cache.items.append(objectsIn: items)
-					cache.timestamp = timestamp
-					realm.add(cache)
-				}
-				completion(nil)
-
-			} catch {
-				completion(error)
+				cache.items.append(objectsIn: items)
+				cache.timestamp = timestamp
+				realm.add(cache)
 			}
+			completion(nil)
+			
+		} catch {
+			completion(error)
 		}
 	}
 
 	public func retrieve(completion: @escaping RetrievalCompletion) {
-		perform { (configuration) in
-			do {
-				let realm = try Realm(configuration: configuration)
-				let caches = realm.objects(Cache.self)
-				guard let cache = caches.first else { return completion(.empty) }
-				completion(.found(feed: cache.items.compactMap(\.feed), timestamp: cache.timestamp))
+		do {
+			let realm = try Realm(configuration: configuration)
+			let caches = realm.objects(Cache.self)
+			guard let cache = caches.first else { return completion(.empty) }
+			completion(.found(feed: cache.items.compactMap(\.feed), timestamp: cache.timestamp))
 
-			} catch {
-				completion(.failure(error))
-			}
+		} catch {
+			completion(.failure(error))
 		}
 	}
 }
